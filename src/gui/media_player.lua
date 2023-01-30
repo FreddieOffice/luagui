de2c109ce2eb88ce2d20f8f925e6b8eeca6358f0
FreddieOffice <70265740+FreddieOffice@users.noreply.github.com>
2023-01-30 19:03:47 +0300

    Add files via upload

---
+++ src/gui/media_player.lua
@@ -0,0 +1,34 @@
+local common = require 'gui.common'
+
+local MediaPlayer = {}
+local metatable = common.create_metatable(MediaPlayer)
+common.add_position(metatable, 'media_player')
+common.add_size(metatable, 'media_player')
+
+function metatable.set_file_name(object, value)
+    object.wx:Load(value)
+end
+
+function MediaPlayer.create(parent)
+  local mediaplayer = {
+     parent = parent,
+     wx_events = {}
+  }
+  
+  mediaplayer.wx = wx.wxMediaCtrl(
+     parent.wx_panel or parent.wx,
+     wx.wxID_ANY,
+     '',
+     wx.wxDefaultPosition, 
+     wx.wxSize(200, 200))
+
+  setmetatable(mediaplayer, metatable)
+  
+  return mediaplayer
+end
+
+function MediaPlayer:play()
+    self.wx:Play()
+end
+
+return MediaPlayer
